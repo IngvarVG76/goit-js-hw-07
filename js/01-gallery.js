92c49c9eb6174f4248a421806bc82861d4e8046e
Igor Grybanov <igor.gribanov@gmail.com>
2023-04-24 14:48:18 +0300

    Task 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,8 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
 //! Завдання 1 - галерея зображень
-// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
+//? Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
 
 // Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
 
@@ -32,8 +35,6 @@
 
 // Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
 
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
 console.log(galleryItems);
 
@@ -59,13 +60,10 @@ const imageItemsEl = galleryItems
   })
   .join("");
 
-// console.log(imageItemsEl); // Check
-
 // 3. Insert html code in to ul
 galleryEl.insertAdjacentHTML("beforeend", imageItemsEl);
 
 // 4. Add basicLightbox and settings
-
 galleryEl.addEventListener("click", (event) => {
   event.preventDefault();
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,62 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+//! Завдання 2 - бібліотека SimpleLightbox
+//? Зроби таку саму галерею як в першому завданні, але використовуючи бібліотеку SimpleLightbox, яка візьме на себе обробку кліків по зображеннях, відкриття і закриття модального вікна, а також гортання зображень за допомогою клавіатури.
+
+// Необхідно трохи змінити розмітку картки галереї, використовуй цей шаблон.
+
+/* <li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li> */
+
+// Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+// Ініціалізація бібліотеки після створення і додання елементів галереї у ul.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+
+//* Solution
+
+// 1. Get galery element
+const galleryEl = document.querySelector(".gallery");
+// console.log(galleryEl); // Check
+
+// 2. Create valid html code for image gallery mapping source array
+const imageItemsEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join("");
+
+// 3. Insert html code in to ul
+galleryEl.insertAdjacentHTML("beforeend", imageItemsEl);
+
+// 4. Create SimpleLightbox and settings
+const lightbox = new SimpleLightbox(".gallery a", {
+  closeText: "&times;",
+  captions: true, // Aneble description on photo
+  captionsData: "alt", // Photo description
+  captionDelay: 250, // Delay of description on photo
+  history: true,
+  enableKeyboard: true, // Arrow navigation and ESC
+  close: true, // add close btn
+});
+
+// 5. Add evt listener and callback func for lightbox
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  lightbox.open(event.target.parentNode.href);
+});
